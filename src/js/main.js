a6c6a4a11fb4aec45faacf8c45fc59edcc00877c
logan dougher <logan.dougher@gmail.com>
2015-09-07 15:58:39 -0500

    changes to color scheme, new greeting logo

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.container').jresponsive({
+    min_size: 50,
+    max_size: 200,
+    hspace: 50,
+    vspace: 10,
+    height: 200,
+    class_name: 'row',
+    transformation: 'animation'
+  });
+});
